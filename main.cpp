e1ffae190f40b2960fbf52e96a7542a6bb39bcbe
ismaelfari1996 <abel_fari@hotmail.com>
2023-06-07 17:30:05 -0500

    edit main

--- main.cpp
+++ main.cpp
@@ -9,30 +9,7 @@ int main()
     stb.imshow("/home/ismael/Documents/programs/gaussfilter_generator/img/charge.jpg",img,width,height);
     kernel gaussianAplyImage=gaussian.gaussianFilter(img,width,height,2.5);
     stb.imshow("/home/ismael/Documents/programs/gaussfilter_generator/img/result.jpg",gaussianAplyImage,width,height);
-   /* kernel g=new double*[4];
-    int cont=1;
-    for (size_t i = 0; i < 4; i++)
-    {
-        g[i]=new double[4];
-        for (size_t j = 0; j < 4; j++)
-        {
-            g[i][j]=cont;
-            cont++;
-        }
-    }
-  kernel r=  gaussian.gaussianFilter(g,4,4,1.3);
-
-  for (size_t i = 0; i < 4; i++)
-  {
-    for (size_t j = 0; j < 4; j++)
-    {
-        printf("%f\t",r[i][j]);
-    }
-    printf("\n");
-  }
-  */
-    
-
+ 
     
     return 0;
 }
